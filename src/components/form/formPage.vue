4f9fff1cc7488693304f312553dc643704a97295
CloudYuXu <xjy.1993@qq.com>
2019-04-25 11:28:12 +0800

    项目初始化

---
+++ src/components/form/formPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <table>
+            <tr>
+                <td>项01</td>
+                <td>项02</td>
+                <td>项03</td>
+                <td>项04</td>
+                <td>项05</td>
+            </tr>
+            <tr>
+                <td>11</td>
+                <td>12</td>
+                <td>13</td>
+                <td>14</td>
+                <td>15</td>
+            </tr>
+            <tr>
+                <td>21</td>
+                <td>22</td>
+                <td>23</td>
+                <td>24</td>
+                <td>25</td>
+            </tr>
+            <tr>
+                <td>31</td>
+                <td>32</td>
+                <td>33</td>
+                <td>34</td>
+                <td>35</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<<script>
+export default {
+    
+}
+</script>
+
+<<style scoped>
+    
+</style>
